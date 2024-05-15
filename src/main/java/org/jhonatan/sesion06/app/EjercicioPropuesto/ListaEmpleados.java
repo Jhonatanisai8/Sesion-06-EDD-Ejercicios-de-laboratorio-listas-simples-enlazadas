acc87582edbc5b27b782ed18f51b313d87a2a681
jho-my <ojedasnachezjhonatan@gmail.com>
2024-05-15 17:03:04 -0500

    Ejercicio 02: Registro de empleados de una compania. agregacion del metodo siEstaVacia y eliminar un nodo especifico en la clase lista empleados

--- src/main/java/org/jhonatan/sesion06/app/EjercicioPropuesto/ListaEmpleados.java
+++ src/main/java/org/jhonatan/sesion06/app/EjercicioPropuesto/ListaEmpleados.java
@@ -23,4 +23,45 @@ public class ListaEmpleados {
         }
         System.out.println("Registrado");
     }
+
+    public boolean estaVacia() {
+        return inicio == null;
+    }
+
+    public boolean eliminarEmpleadoEspecifico(String nombre) {
+
+        boolean encontrado = false;
+        if (!estaVacia()) {
+            //cuando hay un solo nodo
+            if (inicio == fin && nombre.equals(inicio.getEmpleado().getNombre())) {
+                inicio = null;
+                fin = null;
+                encontrado = true;
+            } else if (nombre.equals(inicio.getEmpleado().getNombre())) { // minimo tenemos dos nodos
+                inicio = inicio.siguiente;
+                encontrado = true;
+            } else {
+                //tenemos mas de dos nodos
+                Nodo anterior, temporal;
+                anterior = inicio;
+                temporal = inicio.siguiente;
+
+                while (temporal != null && !temporal.getEmpleado().getNombre().equals(nombre)) {
+                    anterior = anterior.siguiente;
+                    temporal = temporal.siguiente;
+                }
+
+                if (temporal != null) {
+                    anterior.siguiente = temporal.siguiente;
+                    if (temporal == fin) {
+                        fin = anterior;
+                    }
+                    encontrado = true;
+                }
+            }
+        }
+        return encontrado;
+    }
+
+    
 }
