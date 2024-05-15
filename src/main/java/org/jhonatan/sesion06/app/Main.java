81f0c515de7ccea02e5e70f083b603e2bfca9ef2
jho-my <ojedasnachezjhonatan@gmail.com>
2024-05-15 14:24:24 -0500

    Ejercicio 01: Registro de empleados. Diseño del formulario y llamandolo en la clase main

---
+++ src/main/java/org/jhonatan/sesion06/app/Main.java
@@ -0,0 +1,27 @@
+package org.jhonatan.sesion06.app;
+
+import org.jhonatan.sesion06.app.Ejercicio01RegistroEmpleados.frmEmpleados;
+
+/**
+ *
+ * @author Jhonatan
+ */
+public class Main {
+
+    /*
+    Ejercicio 01:
+    Construya una aplicación que permita realizar el registro de empleados
+    donde se podrá guardar, mostrar, consultar, actualizar 
+    y eliminar el registro de empleado. Para todas estas 
+    operaciones considere el ingreso del código del empleado
+     */
+    public static void main(String[] args) {
+        System.out.println("Hola Java!");
+        ejercicioEmpleados();
+    }
+
+    public static void ejercicioEmpleados() {
+        frmEmpleados ventana = new frmEmpleados();
+        ventana.setVisible(true);
+    }
+}
