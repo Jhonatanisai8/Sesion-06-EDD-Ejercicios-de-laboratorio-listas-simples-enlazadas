c83807a01c79ccd6729f28626e5906837f5de65c
jho-my <ojedasnachezjhonatan@gmail.com>
2024-05-15 16:31:30 -0500

    Ejercicio 02: Registro de empleados de una compania.  creando la clase lista empleados con sus punteros, constructor y metodo agregar al inicio

---
+++ src/main/java/org/jhonatan/sesion06/app/EjercicioPropuesto/ListaEmpleados.java
@@ -0,0 +1,26 @@
+package org.jhonatan.sesion06.app.EjercicioPropuesto;
+
+/**
+ *
+ * @author Jhonatan
+ */
+public class ListaEmpleados {
+
+    //punteros
+    protected Nodo inicio, fin;
+
+    //constructor
+    public ListaEmpleados() {
+        this.inicio = null;
+        this.fin = null;
+    }
+
+    //metodo para agregar al inicio;
+    public void agregarInicio(Empleado empleado) {
+        inicio = new Nodo(empleado, inicio);
+        if (fin == null) {
+            fin = inicio;
+        }
+        System.out.println("Registrado");
+    }
+}
