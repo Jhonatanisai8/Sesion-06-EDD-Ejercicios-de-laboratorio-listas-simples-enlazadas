08152e2dc488c0704df95ed3e12032421c3669b9
jho-my <ojedasnachezjhonatan@gmail.com>
2024-05-15 16:30:29 -0500

    Ejercicio 02: Registro de empleados de una compania.  creando la clase nodo con sus atributos y constrcutores

---
+++ src/main/java/org/jhonatan/sesion06/app/EjercicioPropuesto/Nodo.java
@@ -0,0 +1,23 @@
+package org.jhonatan.sesion06.app.EjercicioPropuesto;
+
+/**
+ *
+ * @author Jhonatan
+ */
+public class Nodo {
+
+    private Empleado empleado;
+    public Nodo siguiente;
+
+    //constructor para insertar al inicio
+    public Nodo(Empleado empleado, Nodo nodo) {
+        this.empleado = empleado;
+        siguiente = nodo;
+    }
+
+    public Empleado getEmpleado() {
+        return empleado;
+    }
+
+    
+}
